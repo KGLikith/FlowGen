58971b3d98f0ace95d3bd6eaae4a3a690c6e46cd
KGLikith <glikith1705@gmail.com>
2025-09-16 15:02:52 +0530

    init schema and migrations

---
+++ packages/db/prisma/migrations/20250916093221_init/migration.sql
@@ -0,0 +1,311 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "clerkId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."UserBalance" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "credits" INTEGER NOT NULL,
+
+    CONSTRAINT "UserBalance_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Workflow" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "definition" TEXT,
+    "executionPlan" TEXT,
+    "cron" TEXT,
+    "status" TEXT NOT NULL,
+    "creditsCost" INTEGER NOT NULL,
+    "lastRunAt" TIMESTAMP(3),
+    "lastRunId" TEXT,
+    "lastRunStatus" TEXT,
+    "nextRunAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Workflow_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."WorkflowExecution" (
+    "id" TEXT NOT NULL,
+    "workflowId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "trigger" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "startedAt" TIMESTAMP(3),
+    "completedAt" TIMESTAMP(3),
+    "creditsConsumed" INTEGER NOT NULL,
+
+    CONSTRAINT "WorkflowExecution_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."AvailableTrigger" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "image" TEXT,
+    "category" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "AvailableTrigger_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."AvailableAction" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "image" TEXT,
+    "category" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "AvailableAction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."AvailableTriggerAction" (
+    "id" TEXT NOT NULL,
+    "triggerId" TEXT NOT NULL,
+    "actionId" TEXT NOT NULL,
+
+    CONSTRAINT "AvailableTriggerAction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."ExecutionPhase" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "number" INTEGER NOT NULL,
+    "node" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "startedAt" TIMESTAMP(3),
+    "completedAt" TIMESTAMP(3),
+    "inputs" TEXT,
+    "outputs" TEXT,
+    "creditsConsumed" INTEGER NOT NULL,
+    "workflowExecutionId" TEXT NOT NULL,
+    "triggerId" TEXT,
+    "actionId" TEXT,
+
+    CONSTRAINT "ExecutionPhase_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."ExecutionLog" (
+    "id" TEXT NOT NULL,
+    "executionPhaseId" TEXT NOT NULL,
+    "logLevel" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ExecutionLog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Credential" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Credential_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."UserPurchase" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "stripeId" TEXT NOT NULL,
+    "description" TEXT,
+    "amount" INTEGER NOT NULL,
+    "currency" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UserPurchase_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."DiscordWebhook" (
+    "id" TEXT NOT NULL,
+    "webhookId" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "guildName" TEXT NOT NULL,
+    "guildId" TEXT NOT NULL,
+    "channelId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "DiscordWebhook_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Slack" (
+    "id" TEXT NOT NULL,
+    "teamId" TEXT NOT NULL,
+    "teamName" TEXT NOT NULL,
+    "botUserId" TEXT NOT NULL,
+    "authedUserId" TEXT NOT NULL,
+    "slackAccessToken" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Slack_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Notion" (
+    "id" TEXT NOT NULL,
+    "accessToken" TEXT NOT NULL,
+    "workspaceId" TEXT NOT NULL,
+    "workspaceName" TEXT NOT NULL,
+    "workspaceIcon" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Notion_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."GoogleDocs" (
+    "id" TEXT NOT NULL,
+    "accessToken" TEXT NOT NULL,
+    "refreshToken" TEXT,
+    "folderId" TEXT,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "GoogleDocs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Connections" (
+    "id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "discordId" TEXT,
+    "slackId" TEXT,
+    "notionId" TEXT,
+    "googleDocsId" TEXT,
+    "userId" TEXT,
+
+    CONSTRAINT "Connections_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkId_key" ON "public"."User"("clerkId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserBalance_userId_key" ON "public"."UserBalance"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AvailableTrigger_key_key" ON "public"."AvailableTrigger"("key");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AvailableAction_key_key" ON "public"."AvailableAction"("key");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AvailableTriggerAction_triggerId_actionId_key" ON "public"."AvailableTriggerAction"("triggerId", "actionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "DiscordWebhook_webhookId_key" ON "public"."DiscordWebhook"("webhookId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "DiscordWebhook_url_key" ON "public"."DiscordWebhook"("url");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "DiscordWebhook_channelId_key" ON "public"."DiscordWebhook"("channelId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Slack_slackAccessToken_key" ON "public"."Slack"("slackAccessToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Notion_accessToken_key" ON "public"."Notion"("accessToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Notion_workspaceId_key" ON "public"."Notion"("workspaceId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "GoogleDocs_accessToken_key" ON "public"."GoogleDocs"("accessToken");
+
+-- AddForeignKey
+ALTER TABLE "public"."UserBalance" ADD CONSTRAINT "UserBalance_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Workflow" ADD CONSTRAINT "Workflow_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."WorkflowExecution" ADD CONSTRAINT "WorkflowExecution_workflowId_fkey" FOREIGN KEY ("workflowId") REFERENCES "public"."Workflow"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."WorkflowExecution" ADD CONSTRAINT "WorkflowExecution_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."AvailableTriggerAction" ADD CONSTRAINT "AvailableTriggerAction_triggerId_fkey" FOREIGN KEY ("triggerId") REFERENCES "public"."AvailableTrigger"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."AvailableTriggerAction" ADD CONSTRAINT "AvailableTriggerAction_actionId_fkey" FOREIGN KEY ("actionId") REFERENCES "public"."AvailableAction"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ExecutionPhase" ADD CONSTRAINT "ExecutionPhase_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ExecutionPhase" ADD CONSTRAINT "ExecutionPhase_workflowExecutionId_fkey" FOREIGN KEY ("workflowExecutionId") REFERENCES "public"."WorkflowExecution"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ExecutionPhase" ADD CONSTRAINT "ExecutionPhase_triggerId_fkey" FOREIGN KEY ("triggerId") REFERENCES "public"."AvailableTrigger"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ExecutionPhase" ADD CONSTRAINT "ExecutionPhase_actionId_fkey" FOREIGN KEY ("actionId") REFERENCES "public"."AvailableAction"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ExecutionLog" ADD CONSTRAINT "ExecutionLog_executionPhaseId_fkey" FOREIGN KEY ("executionPhaseId") REFERENCES "public"."ExecutionPhase"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Credential" ADD CONSTRAINT "Credential_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."UserPurchase" ADD CONSTRAINT "UserPurchase_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."DiscordWebhook" ADD CONSTRAINT "DiscordWebhook_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Slack" ADD CONSTRAINT "Slack_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Notion" ADD CONSTRAINT "Notion_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."GoogleDocs" ADD CONSTRAINT "GoogleDocs_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Connections" ADD CONSTRAINT "Connections_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Connections" ADD CONSTRAINT "Connections_discordId_fkey" FOREIGN KEY ("discordId") REFERENCES "public"."DiscordWebhook"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Connections" ADD CONSTRAINT "Connections_slackId_fkey" FOREIGN KEY ("slackId") REFERENCES "public"."Slack"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Connections" ADD CONSTRAINT "Connections_notionId_fkey" FOREIGN KEY ("notionId") REFERENCES "public"."Notion"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Connections" ADD CONSTRAINT "Connections_googleDocsId_fkey" FOREIGN KEY ("googleDocsId") REFERENCES "public"."GoogleDocs"("id") ON DELETE SET NULL ON UPDATE CASCADE;
