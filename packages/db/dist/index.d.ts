1bb5a2666394aaa61825b2e8e0a629e6b9ef33ee
KGLikith <glikith1705@gmail.com>
2025-09-23 17:25:02 +0530

    exported types

--- packages/db/dist/index.d.ts
+++ packages/db/dist/index.d.ts
@@ -1,3 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-export declare const prisma: PrismaClient<import(".prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library").DefaultArgs>;
+import { PrismaClient, Prisma } from "@prisma/client";
+export declare const prisma: PrismaClient<Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library").DefaultArgs>;
+export type { Prisma } from "@prisma/client";
+export * from "@prisma/client";
 //# sourceMappingURL=index.d.ts.map
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 
+// Prisma instance
 export const prisma = new PrismaClient();
+
+// Export Prisma types too
+export type { Prisma } from "@prisma/client";
+export * from "@prisma/client"; // optional, re-export all models/types
