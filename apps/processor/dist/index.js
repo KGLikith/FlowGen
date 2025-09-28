20a745c33c8c4284718cc89319e3eb29ce1b46d8
KGLikith <glikith1705@gmail.com>
2025-09-28 11:40:20 +0530

    Processing the outbox events

--- apps/processor/dist/index.js
+++ apps/processor/dist/index.js
@@ -10,17 +10,53 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const kafkajs_1 = require("kafkajs");
+const db_1 = require("@automation/db");
 const TOPIC_NAME = "workflow-events";
 const kafka = new kafkajs_1.Kafka({
-    clientId: 'processor',
-    brokers: ['localhost:9092']
+    clientId: "processor",
+    brokers: ["localhost:9092"],
 });
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         const producer = kafka.producer();
         yield producer.connect();
-        while (1) {
-            yield new Promise(r => setTimeout(r, 3000));
+        try {
+            while (true) {
+                const outbox = yield db_1.prisma.executionOutbox.findMany({
+                    where: {},
+                    take: 10,
+                });
+                if (outbox.length === 0) {
+                    // nothing to process, wait a bit
+                    yield new Promise((r) => setTimeout(r, 3000));
+                    continue;
+                }
+                console.log("📦 Pending rows", outbox.length);
+                yield producer.send({
+                    topic: TOPIC_NAME,
+                    messages: outbox.map((ex) => ({
+                        key: ex.id.toString(),
+                        value: JSON.stringify({
+                            executionId: ex.workflowExecId,
+                            stage: 1,
+                        }),
+                    })),
+                });
+                yield db_1.prisma.executionOutbox.deleteMany({
+                    where: {
+                        id: {
+                            in: outbox.map((o) => o.id),
+                        },
+                    },
+                });
+            }
+        }
+        catch (err) {
+            console.error("❌ Error in producer loop:", err);
+        }
+        finally {
+            // ✅ flush pending messages before shutdown
+            yield producer.disconnect();
         }
     });
 }
--- apps/processor/src/index.ts
+++ apps/processor/src/index.ts
@@ -1,21 +1,57 @@
-import {Kafka} from "kafkajs";
+import { Kafka } from "kafkajs";
+import { prisma } from "@automation/db";
 
-const TOPIC_NAME = "workflow-events"
+const TOPIC_NAME = "workflow-events";
 
 const kafka = new Kafka({
-    clientId: 'processor',
-    brokers: ['localhost:9092']
-})
+  clientId: "processor",
+  brokers: ["localhost:9092"],
+});
 
 async function main() {
-    const producer =  kafka.producer();
-    await producer.connect();
+  const producer = kafka.producer();
+  await producer.connect();
 
-    while(1) {
-        
+  try {
+    while (true) {
+      const outbox = await prisma.executionOutbox.findMany({
+        where: {},
+        take: 10,
+      });
 
-        await new Promise(r => setTimeout(r, 3000));
+      if (outbox.length === 0) {
+        // nothing to process, wait a bit
+        await new Promise((r) => setTimeout(r, 3000));
+        continue;
+      }
+
+      console.log("📦 Pending rows", outbox.length);
+
+      await producer.send({
+        topic: TOPIC_NAME,
+        messages: outbox.map((ex) => ({
+          key: ex.id.toString(), 
+          value: JSON.stringify({
+            executionId: ex.workflowExecId,
+            stage: 1,
+          }),
+        })),
+      });
+
+      await prisma.executionOutbox.deleteMany({
+        where: {
+          id: {
+            in: outbox.map((o) => o.id),
+          },
+        },
+      });
     }
+  } catch (err) {
+    console.error("❌ Error in producer loop:", err);
+  } finally {
+    // ✅ flush pending messages before shutdown
+    await producer.disconnect();
+  }
 }
 
 main();
